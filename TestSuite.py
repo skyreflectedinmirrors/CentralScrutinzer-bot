e123bb8e941db61902fe8980d7c00c9c66d8f355
arghdos <arghdos@users.noreply.github.com>
2014-07-20 16:07:35 -0400

    Added wiki and blacklist tests
    Signed-off-by: arghdos <arghdos@users.noreply.github.com>

--- TestSuite.py
+++ TestSuite.py
@@ -11,6 +11,7 @@ import Actions as a
 import globaldata as g
 import utilitymethods as u
 import DataExtractors
+import Blacklist
 
 def store_post(post):
     global my_post
@@ -78,34 +79,16 @@ def testUnBanUser(sub, user):
 def testYoutubeExtractor(credentials):
     y = DataExtractors.YoutubeExtractor(credentials['GOOGLEID'])
     id_to_response = {
-        "https://www.youtube.com/watch?v=-vihDAj5VkY": "UC9TOJlW5ZLaiWdMjAUoTpqQ",
-        "https://m.youtube.com/watch?v=G4ApQrbhQp8": "UCKy1dAqELo0zrOtPkf0eTMw",
-        "http://youtu.be/Cg9PWSHL4Vg": "UCEvxaotno5fO12ylDsV1hpQ",
+        "https://www.youtube.com/watch?v=-vihDAj5VkY": "arghdos",
+        "https://m.youtube.com/watch?v=G4ApQrbhQp8": "IGN",
+        "http://youtu.be/Cg9PWSHL4Vg": "Karen Jones",
         "https://www.youtube.com/watch?v=iMoNJ_UiRQY": "PRIVATE",
-        "https://www.youtube.com/watch?v=WkqziN8F8oM": "UCEsBZQzpSmHbzFTUva00Wlw"
-    }
-    channel_to_name = {
-        "UC9TOJlW5ZLaiWdMjAUoTpqQ": "arghdos",
-        "UCKy1dAqELo0zrOtPkf0eTMw": "IGN",
-        "UCEvxaotno5fO12ylDsV1hpQ": "Karen Jones",
-        "UCEsBZQzpSmHbzFTUva00Wlw": "BBrucker2"
-    }
-
-    url_to_name = {
-        "https://www.youtube.com/watch?v=iMoNJ_UiRQY": "PRIVATE",
+        "https://www.youtube.com/watch?v=WkqziN8F8oM": "BBrucker2"
     }
 
     for id, response in id_to_response.iteritems():
         print id, response
         if y.channel_id(id) != response:
-            return False
-
-    for channel, id in channel_to_name.iteritems():
-        if y.channel_name(channel) != channel_to_name[channel]:
-            return False
-
-    for url, id in url_to_name.iteritems():
-        if y.channel_name_url(url) != url_to_name[url]:
             return False
 
     return True
@@ -122,7 +105,7 @@ def testSoundcloudExtractor(credentials):
 
     for id, response in id_to_response.iteritems():
         print id, response
-        if y.channel_name_url(id) != response:
+        if y.channel_id(id) != response:
             return False
     return True
 
@@ -132,14 +115,66 @@ def testBandcampExtractor(credentials):
     id_to_response = {
         "http://wayneszalinski.bandcamp.com/": "wayneszalinski.bandcamp.com",
         "http://www.sleepwalkersbandcamp.bandcamp.com/": "sleepwalkersbandcamp.bandcamp.com",
-        "http://rivka.bandcamp.com/track/better-days": "rivka.bandcamp.com"
+        "http://rivka.bandcamp.com/track/better-days": "rivka.bandcamp.com",
+        "jghkgkjgjhjhg.com": "jghkgkjgjhjhg.com",
+        "jghkgkjgjhjhg": "jghkgkjgjhjhg",
+        "http://rivka.bandcamp.com/track/better-days/https://www.youtube.com/watch?v=RVLwCLGz5hM": "rivka.bandcamp.com"
     }
 
     for id, response in id_to_response.iteritems():
         print id, response
-        if y.channel_name_url(id) != response:
+        if y.channel_id(id) != response:
             return False
     return True
+
+def test_create_wiki(reddit, sub, page):
+    wiki = a.get_or_create_wiki(reddit, sub, page)
+    print "Wiki get/create: " + str(wiki != None)
+    return wiki
+
+def test_write_wiki(wiki):
+    write_test = a.write_wiki_page(wiki, "test")
+    print "Wiki write: " + str(write_test)
+    return write_test
+
+def test_get_wiki(wiki):
+    read_test = a.get_wiki_content(wiki)
+    print "Wiki write: " + str(read_test == "test")
+    return read_test == "test"
+
+def test_black_list(credentials):
+    y = DataExtractors.YoutubeExtractor(credentials['GOOGLEID'])
+    ids = ["https://www.youtube.com/watch?v=-vihDAj5VkY", "https://www.youtube.com/watch?v=AkzOLOih0HA"]
+    blist = Blacklist.Blacklist(credentials, y)
+    if not blist:
+        print "Blacklist creation: False"
+        return False
+    print "Blacklist creation: True"
+
+    #test adding to blacklist
+    blist.add_blacklist(ids[0])
+    check = blist.check_blacklist(ids[1])
+    if not check:
+        print "Blacklist addition: False"
+    print "Blacklist addition: True"
+
+    #test channels
+    channels = blist.get_blacklisted_channels("arghdos")
+    if len(channels) > 0 and channels[0] == "arghdos":
+        print "Blacklist channel get: True"
+    else:
+        print "Blacklist channel get: False"
+        return False
+
+    #test blacklist removal
+    blist.remove_blacklist_url(ids[0])
+    check = blist.check_blacklist(ids[0])
+    if check:
+        print "Blacklist removal: False"
+        return False
+    print "Blacklist removal: True"
+    return True
+
 
 
 def main():
@@ -158,6 +193,12 @@ def main():
     r = u.create_praw(credentials)
 
     sub = r.get_subreddit(credentials['SUBREDDIT'])
+
+    wiki = test_create_wiki(r, sub, "test")
+    test_write_wiki(wiki)
+    test_get_wiki(wiki)
+
+    test_black_list(credentials)
 
     #run MakePost test
     testMakePostText(sub)
