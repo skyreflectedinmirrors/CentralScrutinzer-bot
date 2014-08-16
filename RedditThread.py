0438fd9c6c5e9f0622ab2ea284fc1b32e48db6f9
arghdos <arghdos@users.noreply.github.com>
2014-08-16 17:38:30 -0400

    added reddit thread class handles status checks and errors
    Signed-off-by: arghdos <arghdos@users.noreply.github.com>

---
+++ RedditThread.py
@@ -0,0 +1,38 @@
+import threading
+import CentralScrutinizer
+import Policies
+class RedditThread(object):
+    """
+    A class designed to be used as a thread in the Central Scrutinizer
+    takes care of some basics such as pausing / exiting etc.
+
+    :type owner: CentralScrutinizer.CentralScrutinizer
+    :type policy: Policies.DefaultPolicy
+    """
+    def __init__(self, owner, policy):
+        self.owner = owner
+        self.policy = policy
+        self.wait = threading.Event()
+        self.exit = threading.Event()
+        self.instances = [0 for i in range(policy.Errors_Before_Halt)]
+
+    def __check_status(self):
+        #push back an instance
+        self.instances.append(0)
+        self.instances = self.instances[:self.policy.Errors_Before_Halt]
+        #check for pause
+        while self.wait.is_set():
+            self.wait.wait(self.policy.Pause_Period)
+        #check for exit
+        if self.exit.is_set():
+            self.__shutdown()
+            return False
+        return True
+
+    def __log_error(self):
+        self.instances[-1] == 1
+        if sum(self.instances) == self.policy.Errors_Before_Halt:
+            self.owner.request_pause()
+
+    def run(self):
+        raise NotImplementedError
