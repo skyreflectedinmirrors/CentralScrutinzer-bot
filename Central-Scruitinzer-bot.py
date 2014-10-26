63fd327f70f7069319d151f433271f0e5570a1dd
arghdos <arghdos@users.noreply.github.com>
2014-10-26 10:26:33 -0400

    added main method
    Signed-off-by: arghdos <arghdos@users.noreply.github.com>

--- Central-Scruitinzer-bot.py
+++ Central-Scruitinzer-bot.py
@@ -4,8 +4,20 @@
 # Central-Scruitinzer-bot.py
 #main program for /r/listentothis's centralscrutinizer
 
+import CentralScrutinizer
+import CredentialsImport
+import Policies
+import utilitymethods as u
+
 def main():
-    pass
+    cred = CredentialsImport.CRImport("testcredentials.cred")
+    mypraw = u.create_multiprocess_praw(cred)
+    cred['SUBREDDIT'] = 'listentothis'
+    wz = u.get_subreddit(cred, mypraw, "thewhitezone")
+    pol = Policies.DebugPolicy(wz)
+    cs = CentralScrutinizer.CentralScrutinizer(cred, pol, "database.db")
+    #cs.run()
+    cs.ss.run()
 
 if __name__ == "__main__":
     main()
