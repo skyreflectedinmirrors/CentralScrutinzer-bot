3cd63e951656a48071a2dd14d8f6b631ad8b6399
arghdos <arghdos@users.noreply.github.com>
2015-01-09 13:23:35 -0500

    Signed-off-by: arghdos <arghdos@users.noreply.github.com>

--- Policies.py
+++ Policies.py
@@ -23,11 +23,22 @@ class DefaultPolicy(object):
     Blacklist_Query_Period -- the time to wait between BlacklistQuery scans
     debug_info -- a method that writes debug messages to a desired place
     """
+    def __init__(self, homesub):
+        self.homesub = homesub
+
+    def remove_and_post(self, post):
+        Actions.remove_post(post)
+        Actions.make_comment(post,
+             'This channel has been blacklisted on /r/{}.  For more details '
+             'please [contact the moderators](http://www.reddit.com/message/compose?to=/r/{}&subject='
+             'Why is this channel blacklisted?)  \n'
+             '[^(Don\'t blame me, I\'m just a bot)](/r/centralscrutinizer)'.format(self.homesub, self.homesub))
+
     Historical_Scan_On_New_Database = True
     Historical_Scan_On_Startup = True
     Scan_Sub_Period = 5 * 60 #seconds
     Pause_Period = 2 * 60 #seconds
-    Posts_To_Load = 10
+    Posts_To_Load = 25
     Unread_To_Load = 10
     Mod_Update_Period = datetime.timedelta(days=1)
     Threads = 4
@@ -35,12 +46,13 @@ class DefaultPolicy(object):
     Retry_Multiplier = 2
     Errors_Before_Halt = 3
     Blacklist_Query_Period = 60 #seconds
-    Strike_Counter_Scan_History = datetime.timedelta(days=45) #go back 45 days
+    Strike_Counter_Scan_History = datetime.timedelta(days=180) #go back 180 days
     Strike_Counter_Frequency = 12 * 60 * 60 #every 12 hrs
     on_blacklist = Actions.remove_post
     on_whitelist = Actions.approve_post
     Strike_Count_Max = 3 #three strikes, and you're out
     Use_Reddit_Analytics_For_Historical_Scan = True #much more detailed history (normally)
+    Historial_Scan_Period = 24 * 60 * 60 # 1 day
 
     def debug(self, message, text=u""):
         logging.debug(message + u"\t" + text)
@@ -55,7 +67,8 @@ class DefaultPolicy(object):
         logging.debug(message + u"\t" + url)
 
 class DebugPolicy(DefaultPolicy):
-    def __init__(self, altsub):
+    def __init__(self, homesub, altsub):
+        super(DebugPolicy, self).__init__(homesub)
         self.on_blacklist = lambda post: Actions.xpost(post, altsub, "blacklist")
         self.on_whitelist = lambda post: Actions.xpost(post, altsub, "whitelist")
         self.altsub = altsub
